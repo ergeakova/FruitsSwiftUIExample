7eea4cadba82729ea9a5fc16d9b01ca3d981aab1
ergeakova <ergeak@gmail.com>
2022-09-21 22:53:09 +0300

    added FruitRowView

---
+++ FruitsSwiftUIExample/View/FruitRowView.swift
@@ -0,0 +1,52 @@
+//
+//  FruitRowView.swift
+//  FruitsSwiftUIExample
+//
+//  Created by Erge Gevher Akova on 21.09.2022.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack{
+            
+            // MARK: IMAGE
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: Color(red:0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 3, y: 2)
+                .background(.linearGradient(Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
+                .cornerRadius(8) 
+            
+            // MARK: TEXT
+            VStack(alignment: .leading, spacing: 5){
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
+            
+        } // MARK: - HSTACK
+    }
+}
+
+// MARK: PREVIEW
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView(fruit: fruitsData[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
